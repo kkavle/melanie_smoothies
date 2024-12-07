545858788265c259ef37512cdd6d9a4f0aea7fe7
Karishma Kavle <kkavle@users.noreply.github.com>
2024-12-06 22:42:33 -0800

    Update streamlit_app.py
    corrected session

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write(
 )
 cnx =st.connection("snowflake")
 session = cnx.session()
-session = get_active_session()
+# session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options"). select(col('fruit_name'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
