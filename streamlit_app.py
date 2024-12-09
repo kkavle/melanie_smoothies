64d0470dc2b6104d5039f79ff5d331c83cdd2920
Karishma Kavle <kkavle@users.noreply.github.com>
2024-12-09 10:26:02 -0800

    Update streamlit_app.py JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,4 @@ if time_to_insert:
 
 import requests
 smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
