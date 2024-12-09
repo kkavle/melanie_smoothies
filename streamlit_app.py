3fe13ffb822aa23bc1aa2f22fb4c90d2177c33f1
Karishma Kavle <kkavle@users.noreply.github.com>
2024-12-09 10:02:17 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ session = cnx.session()
 # session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options"). select(col('fruit_name'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
-
+# ingredients_string=''
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe)
 if ingredients_list:
     ingredients_string=''
@@ -23,9 +23,9 @@ if ingredients_list:
     # st.write("""insert inti smoothies.public.order(ingredients) 
     # values ('""" + ingredients_string + """')  """)
 
-time_to_insert = st.button('Submit order')
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    time_to_insert = st.button('Submit order')
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+                values ('""" + ingredients_string + """')"""
 
 # st.write(my_insert_stmt)
 if time_to_insert:
