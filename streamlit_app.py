d9fe510c2babb6703d00fdf98b995daecce3f0ea
Karishma Kavle <kkavle@users.noreply.github.com>
2024-12-09 10:18:51 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ session = cnx.session()
 # session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options"). select(col('fruit_name'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
-# ingredients_string=''
+ingredients_string=''
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe)
 if ingredients_list:
     ingredients_string=''
@@ -23,15 +23,15 @@ if ingredients_list:
     # st.write("""insert inti smoothies.public.order(ingredients) 
     # values ('""" + ingredients_string + """')  """)
 
-    time_to_insert = st.button('Submit order')
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-                values ('""" + ingredients_string + """')"""
+time_to_insert = st.button('Submit order')
+my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+            values ('""" + ingredients_string + """')"""
 
-    # st.write(my_insert_stmt)
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect();
-        st.success('Your Smoothie is order!')
+# st.write(my_insert_stmt)
+if time_to_insert:
+    session.sql(my_insert_stmt).collect();
+    st.success('Your Smoothie is order!')
 
 import requests
 smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+st.text(smoothiefroot_response)
